e7635fea6898d02da7df031d18ea9d7d0a729c53
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-09-25 22:59:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+btn.addEventListener("click", () => {
+    let age = document.querySelector("#age").value;
+    let name = document.querySelector("#name").value;
+
+    let promise = new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+
+            if (age > 18) {
+                resolve();
+            } else {
+                reject();
+            }
+
+        }, 4000);
+
+    });
+
+    promise
+        .then(() => {
+            alert(`Welcome, ${name}. You can vote.`);
+        })
+        .catch(() => {
+            alert(`Oh sorry ${name}. You aren't old enough.`);
+        });
+});
